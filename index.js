de5d9fbf8ca973891159e25569e97d1878d3ad33
Bernát Kalló <kallo.bernat@gmail.com>
2015-08-06 12:08:51 +0000

    added js

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 var postcss = require('postcss');
 
+function JS() {
+   this.fragments = []; 
+}
+
+JS.prototype.toString = function() {
+    return this.fragments.join("\n");
+};
+
+JS.prototype.append = function(f) {
+    this.fragments.push(f);
+    return this;
+};
+
+JS.prototype.prepend = function(f) {
+    this.fragments.unshift(f);
+    return this;
+};
+
 module.exports = postcss.plugin('postcss-javascript-output', function (opts) {
     opts = opts || {};
     
     // Work with options here
 
     return function (css, result) {
-        result.js = 
-
-        // Transform CSS AST here
-
+        result.js = new JS;
     };
 });
